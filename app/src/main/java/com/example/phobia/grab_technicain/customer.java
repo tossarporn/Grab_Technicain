4b8939fa86a60c5228040736d81769c92e6e64f8
tossarporn <tossarporn@gmail.com>
2017-07-04 16:06:24 +0700

    Install GoogleMap Success

--- app/src/main/java/com/example/phobia/grab_technicain/customer.java
+++ app/src/main/java/com/example/phobia/grab_technicain/customer.java
@@ -6,9 +6,20 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 
-public class customer extends AppCompatActivity  {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class customer extends AppCompatActivity implements OnMapReadyCallback {
     private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -37,54 +48,54 @@ public class customer extends AppCompatActivity  {
         setContentView(R.layout.activity_customer);
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_view);
         bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
-//        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
-//        mapFragment.getMapAsync(this);
-    }
+       SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+       mapFragment.getMapAsync(this);
+ }
 
-//    @Override
-//    public void onMapReady(GoogleMap googleMap) {
-//        GoogleMap mMap = googleMap;
-//
-//        Double lat = 13.757792;
-//        Double lng = 100.492984;
-//        LatLng Bangkok = new LatLng(lat, lng);
-//        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-13.757792, 100.492984);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-//        myconfig config = new myconfig();
-//        try {
-//            get_data allmap = new get_data(getApplicationContext());
-//            allmap.execute(config.getShow_all_marker());
-//            String jsonresMap = allmap.get().toString();
-//            JSONArray jsonArray = new JSONArray(jsonresMap);
-//
-//            Log.d("map","get json =>"+jsonresMap+" size"+jsonArray.length());
-//
-//            for (int i =0 ;i<jsonArray.length();i++ ){
-//
-//                JSONObject json_map_tec = jsonArray.getJSONObject(i);
-//                Log.d("map","get json =>"+json_map_tec.getString("name_store").toString());
-//                Double lat_loca = json_map_tec.getDouble("lat");
-//                Double lng_loca = json_map_tec.getDouble("lng");
-//                LatLng location_store = new LatLng(lat_loca, lng_loca);
-//                String name_store = json_map_tec.getString("name_store");
-//                String type = json_map_tec.getString("type_name");
-//                String area = json_map_tec.getString("area_name");
-//                mMap.addMarker(new MarkerOptions().position(location_store).title(name_store).snippet("รับซ้อม "+type+" ("+area+")"));
-//            }
-//
-//        }catch (Exception e){
-//            Log.d("map","get json =>"+e.toString());
-//        }
-//        // Add a marker
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(Bangkok));
-//
-//        //Zoom
-//        mMap.animateCamera(CameraUpdateFactory
-//                .newLatLngZoom(Bangkok, 9));
-//
-//    }
+   @Override
+   public void onMapReady(GoogleMap googleMap) {
+       GoogleMap mMap = googleMap;
+
+       Double lat = 13.757792;
+       Double lng = 100.492984;
+       LatLng Bangkok = new LatLng(lat, lng);
+       // Add a marker in Sydney and move the camera
+       LatLng sydney = new LatLng(-13.757792, 100.492984);
+       mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+       mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+       myconfig config = new myconfig();
+       try {
+           get_data allmap = new get_data(getApplicationContext());
+           allmap.execute(config.getShow_all_marker());
+           String jsonresMap = allmap.get().toString();
+           JSONArray jsonArray = new JSONArray(jsonresMap);
+
+           Log.d("map","get json =>"+jsonresMap+" size"+jsonArray.length());
+
+           for (int i =0 ;i<jsonArray.length();i++ ){
+
+               JSONObject json_map_tec = jsonArray.getJSONObject(i);
+               Log.d("map","get json =>"+json_map_tec.getString("name_store").toString());
+               Double lat_loca = json_map_tec.getDouble("lat");
+               Double lng_loca = json_map_tec.getDouble("lng");
+               LatLng location_store = new LatLng(lat_loca, lng_loca);
+               String name_store = json_map_tec.getString("name_store");
+               String type = json_map_tec.getString("type_name");
+               String area = json_map_tec.getString("area_name");
+               mMap.addMarker(new MarkerOptions().position(location_store).title(name_store).snippet("รับซ้อม "+type+" ("+area+")"));
+           }
+
+       }catch (Exception e){
+           Log.d("map","get json =>"+e.toString());
+       }
+       // Add a marker
+       mMap.moveCamera(CameraUpdateFactory.newLatLng(Bangkok));
+
+       //Zoom
+       mMap.animateCamera(CameraUpdateFactory
+               .newLatLngZoom(Bangkok, 9));
+
+   }
 }
 
 
